b1ed81e907ef4c1b89af51c0b7bd479f558be7e7
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2023-04-08 13:23:46 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,46 +1,30 @@
 <?php
-require_once 'head.php';
-require_once 'nav.php';
+require_once 'master.php';
+master :: head();
+master :: navbar();
 ?>
 <html>
-<div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="1000">
+    <div class="carousel-item active">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/ArtAndFeminism_MoMA18_-_30_-_Editing_with_Megan_Wacha.jpg/1024px-ArtAndFeminism_MoMA18_-_30_-_Editing_with_Megan_Wacha.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Live Support</h5>
-        <p>Tsugicloud offers live support across different organizations.You are on the right hands, we got you covered</p>
-      </div>
     </div>
-    <div class="carousel-item" data-bs-interval="1000">
+    <div class="carousel-item">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/200131-D-RQ659-0001.JPG/1024px-200131-D-RQ659-0001.JPG" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Launch your growth</h5>
-        <p>With Tsugicloud you are always a step head and your productivity grows like a rocket</p>
-      </div>
     </div>
-    <div class="carousel-item" data-bs-interval="1000">
+    <div class="carousel-item">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Junction_2015.jpg/1024px-Junction_2015.jpg" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Open Community</h5>
-        <p>Tsugicloud is powered by an enthusiastic developers community. You can become one too.</p>
-      </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
-</div> 
+</div>
 <br>
 <div class="container text-center">
   <div class="row align-items-center">
@@ -87,7 +71,7 @@ require_once 'nav.php';
 </div>
 <br>
 <?php
-require_once 'footer.php'; 
+master::footer(); 
 ?>
 
 </html>
